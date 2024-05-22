5024a7b1a430074a3adf3b03e612ef7cbb017db3
Anggita Najmi <167683416+gitaa001@users.noreply.github.com>
2024-05-22 23:36:54 +0700

    Update F16_Exit.py

--- F16_Exit.py
+++ F16_Exit.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from F15_Save import *
 
 def exit_program(user_id, user_data, monster, monster_inventory, item_inventory, monster_shop, item_shop):
@@ -17,23 +18,22 @@ def exit_program(user_id, user_data, monster, monster_inventory, item_inventory,
                     monsters_save(monster_shop, folder)
 
                     print("File telah disimpan.")
-                    print(f"Anda akan keluar dari program. Sayonara Agent {user_data['username']}!")
-
+                
                     if os.name == 'nt':
                         os.system('cls')
 
-                    break
+                    print(f"Anda akan keluar dari program. Sayonara Agent {user_data['username']}!")
+                    sys.exit()  # Mengakhiri program
 
                 else:
                     print("Nama folder tidak valid. Silakan coba lagi.")
 
             elif save_confirmation.lower() == "n":
-                print(f"Anda akan keluar dari program. Sayonara Agent {user_data['username']}!")
-                
                 if os.name == 'nt':
                     os.system('cls')
 
-                break
+                print(f"Anda akan keluar dari program. Sayonara Agent {user_data['username']}!")
+                sys.exit()  # Mengakhiri program
 
             else:
                 print("Pilihan tidak valid. Silakan coba lagi.")
