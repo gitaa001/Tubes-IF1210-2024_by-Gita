56fd8af08e7d013635dfe2b6d2ffecbcb8e7acf5
Anggita Najmi <167683416+gitaa001@users.noreply.github.com>
2024-05-23 01:30:09 +0700

    Update F12_ShopManagement.py

--- F12_ShopManagement.py
+++ F12_ShopManagement.py
@@ -17,6 +17,7 @@ def not_monster(monster, monster_shop):
             not_monster_shop.update(tidak_ada)
             
 def not_item(item_inventory, item_shop):
+    global not_item_shop
     for id, data in item_inventory.items():
         ada = False
         for iditem, dataitem in item_shop.items():
@@ -26,7 +27,7 @@ def not_item(item_inventory, item_shop):
         if not ada:
             type = data['type']
             tidak_ada = {(id):{'type': (type)}}
-            not_item_shop = {}
+            not_item_shop.update(tidak_ada)
 
 def tampilkan_shop(barang, monster_shop, monster, item_shop):
     if barang == 'monster':
@@ -49,30 +50,29 @@ def tampilkan_shop(barang, monster_shop, monster, item_shop):
             stok  = data['stock']
             harga = data['price']
             print(f'{id} | {type} | {stok} | {harga}')
-        
 
 def shop_management(user_data, monster, monster_shop, item_shop):
-    print(f'========== SELAMAT DATANG, MR. {user_data['username']} ==========')
+    print(f'========== SELAMAT DATANG, MR. {user_data["username"]} ==========')
     if user_data['role'] == 'admin':
-        aksi = input('\nPilih aksi (lihat/tambah/ubah/hapus/keluar): ')
-        
         while True:
-            if aksi.lower() == 'lihat':
-                    sub_aksi = input('\nApa yang mau dilihat? (monster/item): ')
+            aksi = input('\nPilih aksi (lihat/tambah/ubah/hapus/keluar): ').lower()
 
-                    if sub_aksi == 'monster':                   
-                        tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
-                        
-                    elif sub_aksi == 'item':
-                        tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
-    
-            elif aksi.lower() == 'tambah':
-                sub_aksi = input('\nApa yang mau ditambah? (monster/item): ')
+            if aksi == 'lihat':
+                sub_aksi = input('\nApa yang mau dilihat? (monster/item): ').lower()
+                if sub_aksi == 'monster':                   
+                    tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
+                elif sub_aksi == 'item':
+                    tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
+                else:
+                    print("Pilihan tidak tersedia!")
+
+            elif aksi == 'tambah':
+                sub_aksi = input('\nApa yang mau ditambah? (monster/item): ').lower()
                 if sub_aksi == 'monster':
                     print('ID | TYPE | ATK POWER | DEF POWER | HP')
                     not_monster(monster, monster_shop)
                     
-                    for id, data in not_monster_shop.items():
+                    for id, data in monster.items():
                         type      = data['type']
                         atk_power = data['atk_power']
                         def_power = data['def_power']
@@ -88,115 +88,99 @@ def shop_management(user_data, monster, monster_shop, item_shop):
                             tambah = {(id):{'stock': (stok), 'price': (harga)}}
                             monster_shop.update(tambah)
                             print('\nITEM BERHASIL DITAMBAHKAN KE SHOP')
-                            break
-                    break
+
 
                 elif sub_aksi == 'item':
                     print('ID | TYPE')
-                    not_item()
+                    not_item(item_inventory={}, item_shop=item_shop)  # Adjust as needed
                     for id, data in not_item_shop.items():
                         type = data['type']
                         print(f'{id} | {type}')
 
                     id_item = input('\nMasukkan ID Item: ')
-                    stok    = input('Masukkan Stok Awal')
+                    stok    = input('Masukkan Stok Awal: ')
                     harga   = input('Masukkan Harga: ')
 
-                    for id, data in not_monster.items():
+                    for id, data in not_item_shop.items():
                         if id_item == id:
                             type   = data['type']
                             tambah = {(id):{'type': (type), 'stock': (stok), 'price': (harga)}}
                             item_shop.update(tambah)
                             print('\nITEM BERHASIL DITAMBAHKAN KE SHOP')
-                    break
+                            break
 
-            elif aksi.lower() == 'ubah':
-                sub_aksi = input('\nApa yang mau diubah? (monster/item): ')
+            elif aksi == 'ubah':
+                sub_aksi = input('\nApa yang mau diubah? (monster/item): ').lower()
                 if sub_aksi == 'monster':
                     tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
-
-                    ubah_stok  = False
-                    ubah_harga = False
 
                     id = input("\nMasukkan ID Monster: ")
                     stok = input("Masukkan Stok Baru: ")
                     harga = input("Masukkan Harga Baru: ")
 
-                    if stok != '':
-                        ubah_stok = True
-                        monster_shop[(id)]['stock'] = (stok)
-                    if harga != '':
-                        ubah_harga = True
-                        monster_shop[(id)]['price'] = (harga)
+                    if stok:
+                        monster_shop[id]['stock'] = stok
+                    if harga:
+                        monster_shop[id]['price'] = harga
 
-                    if ubah_stok:
-                        if ubah_harga:
-                            print(f"Monster telah berhasil diubah dengan stok baru sejumlah {stok} dan dengan harga baru {harga}")
-                        else:
-                            print(f"Monster telah berhasil diubah dengan stok baru sejumlah {stok}")
+                    if stok and harga:
+                        print(f"Monster telah berhasil diubah dengan stok baru sejumlah {stok} dan dengan harga baru {harga}")
+                    elif stok:
+                        print(f"Monster telah berhasil diubah dengan stok baru sejumlah {stok}")
+                    elif harga:
+                        print(f"Monster telah berhasil diubah dengan harga baru {harga}")
                     else:
-                        if ubah_harga:
-                            print(f"Monster telah berhasil diubah dengan harga baru {harga}")
-                        else:
-                            print("Isi Stok/Harga yang ingin diubah")
-                        
-                    break
+                        print("Isi Stok/Harga yang ingin diubah")
 
-                elif sub_aksi == 'item':
-                    tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
-
-                    ubah_stok  = False
-                    ubah_harga = False
-
-                    id    = input("\nMasukkan ID Item: ")
-                    stok  = input("Masukkan Stok Baru: ")
-                    harga = input("Masukkan Harga Baru: ")
-
-                    if stok != '':
-                        ubah_stok = True
-                        item_shop[(id)]['stock'] = (stok)
-                    if harga != '':
-                        ubah_harga = True
-                        item_shop[(id)]['price'] = (harga)
-
-                    if ubah_stok:
-                        if ubah_harga:
-                            print(f"Item telah berhasil diubah dengan stok baru sejumlah {stok} dan dengan harga baru {harga}")
-                        else:
-                            print(f"Item telah berhasil diubah dengan stok baru sejumlah {stok}")
-                    else:
-                        if ubah_harga:
-                            print(f"Item telah berhasil diubah dengan harga baru {harga}")
-                        else:
-                            print("Isi Stok/Harga yang ingin diubah")
-                    break
-
-            elif aksi.lower() == 'hapus':
-                sub_aksi = input('\nApa yang mau dihapus? (monster/item): ')
-                if sub_aksi == 'monster':          
-                    tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
-
-                    id = input("\nMasukkan ID Monster: ")
-                    confirm = input('Apakah anda yakin ingin menghapus Monster dari shop (y/n)? ')
-                    
-                    if confirm == 'y':
-                        del monster_shop[(id)]
-                    else:
-                        next
                 elif sub_aksi == 'item':
                     tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
 
                     id = input("\nMasukkan ID Item: ")
-                    confirm = input('Apakah anda yakin ingin menghapus Item dari shop (y/n)? ')
+                    stok = input("Masukkan Stok Baru: ")
+                    harga = input("Masukkan Harga Baru: ")
+
+                    if stok:
+                        item_shop[id]['stock'] = stok
+                    if harga:
+                        item_shop[id]['price'] = harga
+
+                    if stok and harga:
+                        print(f"Item telah berhasil diubah dengan stok baru sejumlah {stok} dan dengan harga baru {harga}")
+                    elif stok:
+                        print(f"Item telah berhasil diubah dengan stok baru sejumlah {stok}")
+                    elif harga:
+                        print(f"Item telah berhasil diubah dengan harga baru {harga}")
+                    else:
+                        print("Isi Stok/Harga yang ingin diubah")
+
+            elif aksi == 'hapus':
+                sub_aksi = input('\nApa yang mau dihapus? (monster/item): ').lower()
+                if sub_aksi == 'monster':          
+                    tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
+
+                    id = input("\nMasukkan ID Monster: ")
+                    confirm = input('Apakah anda yakin ingin menghapus Monster dari shop (y/n)? ').lower()
                     
                     if confirm == 'y':
-                        del item_shop[(id)]
+                        del monster_shop[id]
+                        print("Monster berhasil dihapus dari shop")
                     else:
-                        next
-            
-            elif aksi == 'keluar':
-                exit = True
-                print('Dadah Mr. Monogram!')
+                        print("Penghapusan dibatalkan")
+                elif sub_aksi == 'item':
+                    tampilkan_shop(sub_aksi, monster_shop, monster, item_shop)
 
-# while not exit:
-#     shop_management()
+                    id = input("\nMasukkan ID Item: ")
+                    confirm = input('Apakah anda yakin ingin menghapus Item dari shop (y/n)? ').lower()
+                    
+                    if confirm == 'y':
+                        del item_shop[id]
+                        print("Item berhasil dihapus dari shop")
+                    else:
+                        print("Penghapusan dibatalkan")
+
+            elif aksi == 'keluar':
+                print('Dadah Mr. Monogram!')
+                break
+
+            else:
+                print("Pilihan tidak tersedia! Silakan coba lagi.")
