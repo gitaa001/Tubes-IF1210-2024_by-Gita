c8ce2030f03e54702c2d5617cb8def4d870d2861
Anggita Najmi <167683416+gitaa001@users.noreply.github.com>
2024-05-22 23:33:59 +0700

    Update F03_Logout.py

--- F03_Logout.py
+++ F03_Logout.py
@@ -1,31 +1,30 @@
-islogin = False  # Pastikan islogin diinisialisasi sebagai global
-original_user_data = None  # Variabel global untuk menyimpan data user sebelum logout
+# Insisiasi login
+islogin = False
+original_user_data = None
 
+#Fungsi logout
 def logout(user_id, user_data):
-    global islogin, original_user_data
-    
-    # Simpan data pengguna sebelum mengatur ke None
-    original_user_data = user_data
-    
-    islogin = False  
+    global islogin
+    islogin = False
     user_id = None
     user_data = None
+    print("Anda telah logout, Sayonara!\n")
+    return user_id, user_data
 
-    print("Anda telah logout, Sayonara!")
-    
-    return user_id, user_data  
-
+# Fungsi untuk memproses logout
 def logging_out(user_id, user_data):
-    global original_user_data
+    global original_user_data, islogin
     
     logged_in = user_id is not None
 
     if logged_in:
         user_id, user_data = logout(user_id, user_data)
+        
     else:
         print("Logout Gagal! Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout.")
 
     # Kembalikan data pengguna ke kondisi semula setelah logout
     user_data = original_user_data
-    
+    islogin = False  # Setelah logout, pastikan islogin diatur kembali ke False
+
     return user_id, user_data
